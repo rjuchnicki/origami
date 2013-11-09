a57aa2324de0573526a6dc2deb7fcbf9f4c631dc
Robert J <robert.juchnicki@gmail.com>
2013-11-08 21:19:39 -0500

    Reads command line inputs, records, and executes them

--- backend/commandio.py
+++ backend/commandio.py
@@ -1,2 +1,22 @@
+"""
+	Bash Shell wrapper for recording commands to present later
+"""
+from commands import getstatusoutput
 
+
+record = []
+
+quit = False
+
+while quit == False:
+	cmd = raw_input(">>> ")
+	if cmd.lower() == "quit" or cmd.lower() == "quit ":
+		quit = True
+	else:
+		record.append(("i", cmd))
+		result = getstatusoutput(cmd)
+		print result[1]
+		record.append(("o", result[1]))
+
+print record
 
