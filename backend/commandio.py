9c7774f6641999c76631a86fdba22e621b317bd7
Robert J <robert.juchnicki@gmail.com>
2013-11-09 12:33:03 -0500

    Added slideshare python api and export script

--- backend/commandio.py
+++ backend/commandio.py
@@ -1,22 +1,20 @@
 """
-	Bash Shell wrapper for recording commands to present later
+	Bash shell wrapper for recording commands to present later
 """
-from commands import getstatusoutput
-
+import commands
 
 record = []
 
 quit = False
-
 while quit == False:
 	cmd = raw_input(">>> ")
-	if cmd.lower() == "quit" or cmd.lower() == "quit ":
+	if cmd.lower() == "quit" or cmd.lower() == "quit " or cmd.lower() == "quit  ":
 		quit = True
 	else:
 		record.append(("i", cmd))
-		result = getstatusoutput(cmd)
-		print result[1]
-		record.append(("o", result[1]))
+		result = commands.getoutput(cmd)
+		print result
+		record.append(("o", result))
 
 print record
 
